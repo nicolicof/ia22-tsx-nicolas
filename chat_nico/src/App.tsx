62a7fe11852045e024b3f8f13dbd1c4ef57b1fbe
nicolasf <106293553+nicolicof@users.noreply.github.com>
2023-11-08 01:29:52 +0000

    sim

---
+++ chat_nico/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import "./App.css"
+import Input from "./Input.tsx"
+import Message from "./Message.tsx"
+
+export default () => {
+  const [mensagens, setMensagem] = useState([
+    { self: true, date: "04:20 2020/12/12", user: "Dunha", text: "lorem ipsum" },
+    { self: false, date: "04:21 2020/12/12", user: "Dunha", text: "lorem ipsum" },
+    { self: false, date: "04:22 2020/12/12", user: "Dunha", text: "lorem ipsum" },
+    { self: true, date: "04:23 2020/12/12", user: "Dunha", text: "lorem ipsum" },
+  ])
+
+  return <>
+    <main>
+      {mensagens.map(({ self, date, user, text }, index) =>
+        <Message self={self} date={date} user={user} text={text} key={index} />)
+      }
+    </main>
+    <Input setMensagem={setMensagem} mensagens={mensagens} />
+  </>
+}
